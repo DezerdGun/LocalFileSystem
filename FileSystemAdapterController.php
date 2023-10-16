653e4d3bf38157f9272bb508a461885dd05ebf28
Defence <mir6raid@gmail.com>
2023-10-16 15:12:38 +0500

    correctSimpleDir

--- FileSystemAdapterController.php
+++ FileSystemAdapterController.php
@@ -1,26 +1,30 @@
 <?php
 
-
+namespace app\components;
 
 use app\models\FileModel;
 
-public function actionIndex()
-{
+class FileSystemAdapterController extends Controller{
+  public function actionIndex()
+  {
     $model = new FileModel();
 
     if ($model->load(Yii::$app->request->post()) && $model->validate()) {
-        $path = '/admin/uploads/.../BankFileExample.jpg';
-        $adapter = Yii::$app->filesystem->getAdapter();
-        if ($model->upload($adapter, $path)) {
-            //here file will be upload successfully
-            return $this->render('success');
-        }
+      $path = '/admin/uploads/.../BankFileExample.jpg';
+      $adapter = Yii::$app->filesystem->getAdapter();
+      if ($model->upload($adapter, $path)) {
+        //here file will be upload successfully
+        return $this->render('success');
+      }
     }
 
     return $this->render('index', ['model' => $model]);
-}
+  }
+
 
 
 
 //  метод getAdapter()
 // прикладного компонента filesystem для получения соответствующего объекта адаптера на основе настроенного параметра $fsType.
+}
+
